7d0119a5417e086cd26395167818bf5b9d7be7fe
rainsound-k <rainsound128@gmail.com>
2018-05-04 10:36:22 +0900

    boto3 및 S3 연결

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,12 +1,15 @@
 from .base import *
 
 secrets = json.loads(open(SECRETS_DEV, 'rt').read())
+set_config(secrets, module_name=__name__, start=True)
 
 DEBUG = True
 ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 INSTALLED_APPS += [
     'django_extensions',
+    'storages',
 ]
 
-set_config(secrets, module_name=__name__, start=True)
+DEFAULT_FILE_STORAGE = 'config.storage.DefaultFileStorage'
+STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
